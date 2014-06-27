bbf5a5d3202540d3273ccc40ac2a4c1f6824cc04
schackv <schackv@users.noreply.github.com>
2014-06-27 11:58:54 +0200

    shape index demo implemented

--- phenoquant_demo/demo_features.py
+++ phenoquant_demo/demo_features.py
@@ -42,17 +42,28 @@ def demo_features():
     im_gray = imtools.standardize(im_gray,mask=mask)
 #    F_bwratio = pq.features.bwratio(im,feature_opts['bwratio'])
     F_gradient, labels_gradient = features.gradient_histograms(im_gray,**feature_opts['gradient_histograms'])
-#    F_shape = pq.features.shape_historgrams(im,feature_opts['shape_histograms'])
+    F_shape, labels_shape = features.shape_histograms(im_gray,**feature_opts['shape_histograms'])
     
     # Gradient orientations
     fig, axs = plt.subplots(2,4)
     for ax, F, label in zip(axs.flat,F_gradient.T,labels_gradient):
         ax.imshow(F.T)
         ax.axis('image')
+        ax.axis('off')
         ax.set_title(label)
     fig.suptitle('Gradient orientation histograms')
-    fig.show()
-
+    plt.show(block=False)
+    
+    # Shape index histograms
+    fig, axs = plt.subplots(len(feature_opts['shape_histograms']['sigmas']),feature_opts['shape_histograms']['nbins'])
+    for ax, F, label in zip(axs.flat,F_shape.T,labels_shape):
+        ax.imshow(F.T)
+        ax.axis('image')
+        ax.axis('off')
+        ax.set_title(label)
+    fig.suptitle('Shape index histograms')
+    
+    plt.show(block=True)
 
 if __name__=='__main__':
     demo_features()
